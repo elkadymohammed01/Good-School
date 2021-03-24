5d1de6809061ccbc4e2c35ebd02f56c97578e033
Mohammed Elkady <76850378+elkadymohammed01@users.noreply.github.com>
2021-03-25 01:58:14 +0200

    Delete Task_MLT directory

--- Task_MLT/app/src/main/java/com/example/task_mlt/data/singletoon.java
+++
@@ -1,7 +0,0 @@
-package com.example.task_mlt.data;
-
-public class singletoon {
-    public static  String name,mail,phone,ser_mail,ser_name,ser_info;
-    public static double  v=31,v1=31;
-    public static boolean show=false;
-}
